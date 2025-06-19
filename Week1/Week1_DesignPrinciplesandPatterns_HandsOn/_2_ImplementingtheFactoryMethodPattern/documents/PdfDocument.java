df77ef1452385a04bcc349f589dc7f9e764192e5
SujithJ05 <sujithchandraj09@gmail.com>
2025-06-19 09:54:09 +0530

    Describe your change here

---
+++ Week1/Week1_DesignPrinciplesandPatterns_HandsOn/_2_ImplementingtheFactoryMethodPattern/documents/PdfDocument.java
@@ -0,0 +1,8 @@
+package Week1.Week1_DesignPrinciplesandPatterns_HandsOn._2_ImplementingtheFactoryMethodPattern.documents;
+
+
+public class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening a PDF document.");
+    }
+}

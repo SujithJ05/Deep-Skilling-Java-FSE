328c2ccbd4981e031f46982b7fbb0a828dc8d3f9
SujithJ05 <sujithchandraj09@gmail.com>
2025-07-27 23:21:32 +0530

    Commit

---
+++ Week6/React Main Program/myfirstreact/src/Home.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import class Home extends Component
+{
+    render()
+    {
+        <div>
+            <h3>Welcome to the Home page of Student Management Portal</h3>
+        </div>
+    }
+}
